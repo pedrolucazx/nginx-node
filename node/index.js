78438396f92fcc3466871da8be06932b1bef861b
pedrolucazx <pedrolucastrainee@gmail.com>
2023-02-01 14:49:26 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql2/promise");
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+async function insertPersonAndRetrieve(name) {
+  const connection = await mysql.createConnection(config);
+  await connection.query(`INSERT INTO people(name) values ('${name}')`);
+  const [rows] = await connection.execute(`SELECT * FROM people WHERE name='${name}'`);
+  connection.end();
+  return rows[0].name;
+}
+
+let personName = '';
+insertPersonAndRetrieve('João Fabio')
+  .then((name) => {
+    personName = name;
+    app.get("/", (req, res) => res.send(`<h1>Full Cycle Rocks! ${personName}</h1>`));
+    app.listen(PORT, () => console.log(`Server is running port ${PORT}`));
+  });
